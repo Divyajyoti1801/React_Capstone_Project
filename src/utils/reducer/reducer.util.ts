57dd7f05158b74f73567c0d12ba103d76033c56f
Divyajyoti <divyajyoti182000@gmail.com>
2023-03-07 16:28:06 +0530

    Implementation of type script + Code Refactoring Under Progress

---
+++ src/utils/reducer/reducer.util.ts
@@ -0,0 +1,26 @@
+import { AnyAction } from "redux";
+
+//Type Predicate
+
+type Matchable<AC extends () => AnyAction> = AC & {
+  type: ReturnType<AC>["type"];
+  match(action: AnyAction): action is ReturnType<AC>;
+};
+
+//Function overloading comes from TypeScript
+
+export function createAction<T extends string, P>(
+  type: T,
+  payload: P
+): ActionWithPayload<T, P>;
+
+export function createAction<T extends string>(
+  type: T,
+  payload: void
+): Action<T>;
+
+export function createAction<T extends string, P>(type: T, payload: P) {
+  return { type, payload };
+}
+
+// export const createAction = (type, payload) => ({ type, payload });
